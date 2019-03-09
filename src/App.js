2c6e1671a78d30ed3b6178509e2bd5a4d4c48c76
hobglobin27 <hobglobin27@gmail.com>
2019-03-09 15:51:40 -0600

    NavBar y logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import Inicio from "./components/Inicio"
-import './App.css';
+import './components/estilos/styles.css';
+import 'antd/dist/antd.css';
 import NavBar from './components/NavBar';
+import Registro from './components/Registro'
+import { Layout} from 'antd';
+
+const { Header, Footer, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar className="fondoNav"/>
-        <Inicio />
-      </div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo">
+            <img className="logoProps" src="https://res.cloudinary.com/dbwsjcrfc/image/upload/v1552166212/imagenes/logo2.png" alt=""/>
+          </div>
+          <NavBar/>
+        </Header>
+        <Content className="fondo">
+          <Inicio />
+        </Content>        
+        <Footer className="fondoFooter" style={{background: "#09182e", color: "#ffffff"}}>
+          Con vocación ©2018 Created by IPA
+        </Footer>
+      </Layout>
+
     );
   }
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
-import { Menu, Icon } from 'antd';
+import { Menu} from 'antd';
 import React from "react";
 import 'antd/dist/antd.css';
 import './estilos/styles.css'
+import FaUserPlus from 'react-icons/lib/fa/user-plus'
+import FaSignIn from 'react-icons/lib/fa/sign-in'
+import MdHelp from 'react-icons/lib/md/help'
 
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
 
 class NavBar extends React.Component {
   state = {
-    current: 'mail',
+    current: '',
   }
 
   handleClick = (e) => {
@@ -26,25 +27,16 @@ class NavBar extends React.Component {
         mode="horizontal"
         theme='dark'
       >
-        <Menu.Item key="mail">
-          <Icon type="mail" />Navigation One
+        <Menu.Item key="user-plus">
+          <FaUserPlus/> Registrate
         </Menu.Item>
-        <Menu.Item key="app">
-          <Icon type="appstore" />Navigation Two
+        <Menu.Item key="sign-in">
+          <FaSignIn/> Ingresa
         </Menu.Item>
-        <SubMenu title={<span className="submenu-title-wrapper"><Icon type="setting" />Navigation Three - Submenu</span>}>
-          <MenuItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </MenuItemGroup>
-          <MenuItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </MenuItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+        <Menu.Item key="help">
+          <MdHelp /> Ayuda
         </Menu.Item>
+        
       </Menu>
     );
   }
